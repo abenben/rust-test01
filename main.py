e0baa7b232f3cbdb3ed915ba2a6504b210150f19
abenben <abenbenben@gmail.com>
2020-08-23 21:30:14 +0900

    関数追加

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
 import test01
 test01.hello()
 print(test01.pi_times(10))
+print(test01.get_length("1234567890"))
+print(test01.multiply_array([1,2,3,4,5,6,7,8],10))
+print(test01.multiply_tuple((1,2),4))
--- src/lib.rs
+++ src/lib.rs
@@ -15,10 +15,28 @@ fn pi_times( n: usize ) -> PyResult<Vec<f64>> {
     )
 }
 
+#[pyfunction]
+fn get_length(a: &str) -> PyResult<usize> {
+    Ok(a.len())
+}
+
+#[pyfunction]
+fn multiply_array(a: Vec<usize>, b: usize) -> PyResult<Vec<usize>> {
+    Ok(a.iter().map(|v| v * b).collect())
+}
+
+#[pyfunction]
+fn multiply_tuple(a: (usize, usize), b: usize) -> PyResult<(usize, usize)> {
+    Ok((a.0 * b, a.1 * b))
+}
+
 #[pymodule]
 fn test01(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_wrapped(wrap_pyfunction!( hello ))?;
     m.add_wrapped(wrap_pyfunction!( pi_times ))?;
+    m.add_wrapped(wrap_pyfunction!( get_length ))?;
+    m.add_wrapped(wrap_pyfunction!( multiply_array ))?;
+    m.add_wrapped(wrap_pyfunction!( multiply_tuple ))?;
 
     Ok(())
 }
